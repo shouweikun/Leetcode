374cbcd5450fdf8e7605173836c2018ee960792f
neighborhood <john_liu@laplace-work.local>
2017-10-13 18:06:21 +0800

    update_2

--- src/main/scala/quiz1_50/Add_Two_Numbers_2.scala
+++ src/main/scala/quiz1_50/Add_Two_Numbers_2.scala
@@ -1,8 +1,14 @@
 package quiz1_50
 
+import Context.ListNode
+
 /**
   * Created by john_liu on 2017/10/13.
   */
 object Add_Two_Numbers_2 {
+
+  def addTwoNumbers(l1: ListNode, l2: ListNode): ListNode = {
+
+  }
 
 }
