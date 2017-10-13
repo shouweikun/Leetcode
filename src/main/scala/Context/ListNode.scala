dca72929614678dd4a443cef580828b766b7dee7
neighborhood <john_liu@laplace-work.local>
2017-10-13 18:05:14 +0800

    add_listnode

---
+++ src/main/scala/Context/ListNode.scala
@@ -0,0 +1,10 @@
+package Context
+
+/**
+  * Created by john_liu on 2017/10/13.
+  */
+class ListNode (var _x: Int = 0) {
+    var next: ListNode = null
+       var x: Int = _x
+     }
+
---
+++ src/main/scala/quiz1_50/Add_Two_Numbers_2.scala
@@ -0,0 +1,8 @@
+package quiz1_50
+
+/**
+  * Created by john_liu on 2017/10/13.
+  */
+object Add_Two_Numbers_2 {
+
+}
