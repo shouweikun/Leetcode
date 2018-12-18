33d2b4d2681ed6d36637d008fe9249b2e2954dfc
neighborhood <liushouwei@finupgroup.com>
2018-12-18 19:39:29 +0800

    279

---
+++ src/main/scala/quiz51_100/UniqueBinarySearchTrees_2_95.java
@@ -0,0 +1,23 @@
+package quiz51_100;
+
+import Context.java.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by john_liu on 2018/12/18.
+ */
+public class UniqueBinarySearchTrees_2_95 {
+//    public List<TreeNode> generateTrees(int n) {
+//        List<TreeNode> res = new LinkedList<>();
+//        for (int i = 1; i <= n; i++) {
+//            for (TreeNode node : res) {
+//
+//            }
+//        }
+//    }
+
+//    private TreeNode copyT
+
+}
