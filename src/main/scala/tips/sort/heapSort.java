366a6c49d8d588b6b12f38f1015db6d6f8d58bbd
neighborhood <liushouwei@finupgroup.com>
2019-02-18 18:40:33 +0800

    heap

--- src/main/scala/tips/sort/heapSort.java
+++ src/main/scala/tips/sort/heapSort.java
@@ -5,34 +5,34 @@ package tips.sort;
  */
 public class HeapSort {
     public static void heapSort(int[] arr) {
-       makeHeap(arr);
-       for(int i =arr.length;i>1;i--){
-           heapAdjust(arr,i,arr.length);
-       }
+        makeHeap(arr);
+        for (int i = 0; i < arr.length; i++) {
+            swap(arr, i, arr.length - 1 - i);
+            heapAdjust(arr, i, arr.length - 1 - i - 1);
+        }
     }
 
     private static void makeHeap(int[] arr) {
-      for(int i= arr.length/2;i>0;i--){
-          heapAdjust(arr,i,arr.length-1);
-      }
+        for (int i = arr.length / 2 + 1; i >= 0; i--) {
+            heapAdjust(arr, i, arr.length - 1);
+        }
     }
 
-    /**
-     *已知arr[start,end]中只有arr[start]不满足堆定义
-     * 是arr【start,end]成为大顶堆
-     * @param arr
-     * @param start
-     * @param end
-     */
     private static void heapAdjust(int[] arr, int start, int end) {
         int temp = arr[start];
-        for(int i = 2*start;i<=end;i*=2){
-            if(i<end && arr[i]> arr[i+1])i++;
-            if(temp>arr[i])break;
-            arr[start] =arr[i];
-            start=i;
-
+        for (int i = 2 * start; i <= end; i *= 2) {
+            if (i < end && arr[i] < arr[i + 1]) i++;
+            if (temp < arr[i]) {
+                arr[start] = arr[i];
+                arr[i] = temp;
+                start = i;
+            } else break;
         }
-        arr[start] = temp;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
