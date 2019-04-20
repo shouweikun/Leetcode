f3348f925426bea2c8a7c65dbf5c4e3b38d81e86
neighborhood <john_liu@liushouweideMBP.lan>
2019-04-20 22:23:03 +0800

    138

--- src/main/scala/quiz101_150/CopyListwithRandomPointer_138.java
+++ src/main/scala/quiz101_150/CopyListwithRandomPointer_138.java
@@ -35,22 +35,43 @@ public class CopyListwithRandomPointer_138 {
                 copy = new Node();
                 copy.val = node.val;
                 dummy.next = copy;
-                copy.random = node.random;
                 map.put(node, copy);
 
             } else {
                 copy.next = new Node();
                 copy.next.val = node.val;
-                copy.random = node.random;
                 map.put(node, copy);
                 copy = copy.next;
             }
             node = node.next;
         }
         copy = dummy.next;
+        node = head;
         while (copy != null) {
-            if (copy.random != null) copy.random = map.get(copy.random);
+            if (node.random != null) copy.random = map.get(copy.random);
+            copy = copy.next;
+            node = node.next;
         }
         return dummy.next;
+    }
+
+
+    public Node copyListWithRandomPointer(Node head) {
+        if (head == null) return null;
+        Map<Node, Node> map = new HashMap<>();
+        Node copyHead = new Node();
+        copyHead.val = head.val;
+        map.put(head, copyHead);
+        for (Node p = head.next, q = copyHead; p != null; p = p.next, q = q.next) {
+            q.next = new Node();
+            q.next.val = p.val;
+            map.put(p, q.next);
+        }
+        for (Node p = head, q = copyHead; p != null; p = p.next, q = q.next) {
+            if (p.random != null) {
+                q.random = map.get(p.random);
+            }
+        }
+        return copyHead;
     }
 }
